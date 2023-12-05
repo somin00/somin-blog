6a624da67dac04261b0633900b647fb6eb40fa22
somin <smhs12@naver.com>
2023-12-05 15:12:02 +0900

    feat: 알림 기능 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navigation />
         <main>{children}</main>
         <Footer />
+        <div id="notifications"></div>
       </body>
     </html>
   );
---
+++ src/components/ui/notification.tsx
@@ -0,0 +1,33 @@
+import { createPortal } from "react-dom";
+
+import styles from "./notification.module.css";
+
+type NotificationType = {
+  title: string;
+  message: string;
+  status: string;
+};
+
+function Notification({ title, message, status }: NotificationType) {
+  let statusStyles = "";
+
+  if (status === "success") {
+    statusStyles = styles.success;
+  }
+
+  if (status === "error") {
+    statusStyles = styles.error;
+  }
+
+  const cssStyles = `${styles.notification} ${statusStyles}`;
+  const notificationArea = document.getElementById("notifications")!;
+  return createPortal(
+    <div className={cssStyles}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>,
+    notificationArea
+  );
+}
+
+export default Notification;
