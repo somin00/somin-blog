003880e90bc3b0c3199fc92f4635a55923144323
somin <smhs12@naver.com>
2023-12-06 12:44:52 +0900

    feat: 알림 상태 객체 상수로 분리

---
+++ src/app/costants/index.ts
@@ -0,0 +1,17 @@
+export const PENDING_NOTIFICATION = Object.freeze({
+  status: "pending",
+  title: "전송중",
+  message: "메세지를 전송중입니다.",
+});
+
+export const SUCCESS_NOTIFICATION = Object.freeze({
+  status: "success",
+  title: "완료",
+  message: "메세지 전송을 완료했습니다.",
+});
+
+export const ERROR_NOTIFICATION = Object.freeze({
+  status: "error",
+  title: "실패",
+  message: "메세지 전송을 실패했습니다.",
+});
