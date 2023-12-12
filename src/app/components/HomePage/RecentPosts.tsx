d42f2a0f7997c26a55bb3e335e6268b175c52d6b
somin <smhs12@naver.com>
2023-12-12 15:07:51 +0900

    style: 최근 게시물에 전체 블로그 글 링크 추가

--- src/app/components/HomePage/RecentPosts.tsx
+++ src/app/components/HomePage/RecentPosts.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Posts } from "../../../../types";
 import PostsContainer from "../Posts/PostsContainer";
 import styles from "./RecentPosts.module.css";
@@ -7,6 +8,9 @@ export default function RecentPosts({ posts }: { posts: Posts }) {
     <section className={styles.latest}>
       <h2>최근 게시글</h2>
       <PostsContainer posts={posts} />
+      <Link href="/posts" className={styles.link}>
+        전체 게시글 보기
+      </Link>
     </section>
   );
 }
