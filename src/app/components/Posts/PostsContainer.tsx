5d9e8aa52fb8ae59cae9d85f9a0fd192fdd6e7b7
somin <smhs12@naver.com>
2024-02-02 01:28:20 +0900

    fix: 빌드 에러 해결

--- src/app/components/Posts/PostsContainer.tsx
+++ src/app/components/Posts/PostsContainer.tsx
@@ -2,8 +2,8 @@ import { Posts } from "../../../../types";
 import PostItem from "./PostItem";
 import styles from "./PostsContainer.module.css";
 
-export default function PostsContainer({ posts, selectedCategory }: { posts: Posts; selectedCategory: string }) {
-  const filteredPosts = posts.filter((post) => post.category.includes(selectedCategory));
+export default function PostsContainer({ posts, selectedCategory }: { posts: Posts; selectedCategory?: string }) {
+  const filteredPosts = posts.filter((post) => post.category.includes(selectedCategory!));
   return (
     <ul className={styles.grid}>
       {selectedCategory
